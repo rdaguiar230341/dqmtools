1175b91149785d7217c968fe816bdb5d35d41658
np04daq <np04daq@np04-srv-024.cern.ch>
2024-02-16 16:25:11 +0100

    make the DQMTestSuite a DQMTest, so we can have better nesting

--- scripts/dqm_analyzer.py
+++ scripts/dqm_analyzer.py
@@ -22,15 +22,15 @@ import click
 def main(filenames, nrecords, nworkers, hd, warm, pds, wibpulser, make_plots):
 
     #setup our tests
-    dqm_test_suite = DQMTestSuite()
-    dqm_test_suite.register_test(CheckAllExpectedFragmentsTest())
-    dqm_test_suite.register_test(CheckNFrames_WIBEth())
+    dqm_test_suite_wibs = DQMTestSuite("WIBEth Tests")
+    dqm_test_suite_wibs.register_test(CheckAllExpectedFragmentsTest())
+    dqm_test_suite_wibs.register_test(CheckNFrames_WIBEth())
     
     if(hd):
         tpc_det_name = "HD_TPC"
         tpc_det_id = 3
         tpc_rms_high_threshold=100
-        tpc_rms_low_threshold=[20.,15.]
+        tpc_rms_low_threshold=[15.]
         if not warm:
             tpc_rms_high_threshold=50
             tpc_rms_low_threshold=[4.,3.]
@@ -44,43 +44,50 @@ def main(filenames, nrecords, nworkers, hd, warm, pds, wibpulser, make_plots):
             tpc_rms_high_threshold=50
             tpc_rms_low_threshold=[2.,3.]
 
-    dqm_test_suite.register_test(CheckTimestampDiffs_WIBEth(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckTimestampDiffs_WIBEth(tpc_det_name))
 
-    dqm_test_suite.register_test(CheckWIBEth_COLDDATA_Timestamp_0_Diff(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_COLDDATA_Timestamp_1_Diff(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_COLDDATA_Timestamps_Aligned(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_COLDDATA_Timestamp_0_Diff(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_COLDDATA_Timestamp_1_Diff(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_COLDDATA_Timestamps_Aligned(tpc_det_name))
 
-    dqm_test_suite.register_test(CheckWIBEth_CRC_Err(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_Pulser(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_Calibration(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_Ready(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_Context(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_CD(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_LOL(tpc_det_name))        
-    dqm_test_suite.register_test(CheckWIBEth_Link_Valid(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_WIB_Sync(tpc_det_name))
-    dqm_test_suite.register_test(CheckWIBEth_FEMB_Sync(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_CRC_Err(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_Pulser(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_Calibration(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_Ready(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_Context(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_CD(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_LOL(tpc_det_name))        
+    dqm_test_suite_wibs.register_test(CheckWIBEth_Link_Valid(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_WIB_Sync(tpc_det_name))
+    dqm_test_suite_wibs.register_test(CheckWIBEth_FEMB_Sync(tpc_det_name))
 
-    dqm_test_suite.register_test(CheckTimestampsAligned(tpc_det_id),f"CheckTimestampsAligned_{tpc_det_name}")
+    dqm_test_suite_wibs.register_test(CheckTimestampsAligned(tpc_det_id),f"CheckTimestampsAligned_{tpc_det_name}")
 
     if(not wibpulser):
-        dqm_test_suite.register_test(CheckRMS_WIBEth(det_name=tpc_det_name,threshold=tpc_rms_high_threshold,verbose=True),
+        dqm_test_suite_wibs.register_test(CheckRMS_WIBEth(det_name=tpc_det_name,threshold=tpc_rms_high_threshold,verbose=True),
                                          name=f"CheckRMS_{tpc_det_name}_High")
-        dqm_test_suite.register_test(CheckRMS_WIBEth(det_name=tpc_det_name,threshold=tpc_rms_low_threshold,operator=operator.lt,verbose=True),
+        dqm_test_suite_wibs.register_test(CheckRMS_WIBEth(det_name=tpc_det_name,threshold=tpc_rms_low_threshold,operator=operator.lt,verbose=True),
                                          name=f"CheckRMS_{tpc_det_name}_Low")
-        dqm_test_suite.register_test(CheckPedestal_WIBEth(det_name=tpc_det_name,verbose=True),
+        dqm_test_suite_wibs.register_test(CheckPedestal_WIBEth(det_name=tpc_det_name,verbose=True),
                                          name=f"CheckPedestal_{tpc_det_name}")
 
+    dqm_test_suite = DQMTestSuite("All Tests")
+    dqm_test_suite.register_test(dqm_test_suite_wibs)
+        
     if pds:
         """
         Create separate test suite for DAPHNE and register all related tests
         """
-        dqm_test_suite_daphne = DQMTestSuite()
+        dqm_test_suite_daphne = DQMTestSuite("DAPHNETests")
         dqm_test_suite_daphne.register_test(CheckTimestampsAligned(2),"CheckTimestampsAligned_PDS")
         dqm_test_suite_daphne.register_test(CheckEmptyFragments_DAPHNE(), "CheckEmptyFragments_DAPHNE")
         dqm_test_suite_daphne.register_test(CheckTimestampDiffs_DAPHNE())
         dqm_test_suite_daphne.register_test(CheckADCData_DAPHNE())
 
+        dqm_test_suite.register_test(dqm_test_suite_daphne)
+
+
+        
     df_dict = {}
     n_processed_records = 0
     for filename in filenames:
@@ -102,15 +109,15 @@ def main(filenames, nrecords, nworkers, hd, warm, pds, wibpulser, make_plots):
 
     df_dict = dfc.concatenate_dataframes(df_dict)
 
-    print(df_dict.keys())
+    #print(df_dict.keys())
 
-    dqm_test_suite.do_all_tests(df_dict)
-    print(dqm_test_suite.get_table())
+    res = dqm_test_suite.run_test(df_dict)
+    print(dqm_test_suite.get_table(show_last_update=False))
 
-    if pds:
-        print("\n\nDAPHNE test results:")
-        dqm_test_suite_daphne.do_all_tests(df_dict)
-        print(dqm_test_suite_daphne.get_table())
+    for test in dqm_test_suite.get_all_tests():
+        if test.is_test_suite():
+            print(f'Results for {test.get_name()}:')
+            print(test.get_table(show_last_update=False))
 
     if(make_plots):
         if(not wibpulser):
